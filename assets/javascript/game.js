87f0c6bce085ac0cdaa483e7f1e77db30157273c
jwescott <jwescott@mail.usf.edu>
2019-10-26 22:21:32 -0400

    progress after tutoring

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,42 +1,88 @@
 document.write("Press any key to get started!")
-        var name = prompt("What's your name?");
-        console.log("Hello " + name);
-
-        var words = ["red", "yellow", "purple", "green", "orange", "blue", "pink"];
-
-        var word = words[Math.floor(Math.random() * words.length)];
-        console.log("Computer Guess " + word)
-
-
-
-
+        // var name = prompt("What's your name?");
+        var wordsList = ["red", "yellow", "purple", "green", "orange", "blue", "pink"];
+        var word = wordsList[Math.floor(Math.random() * wordsList.length)];
         var answerArray = [];
-        for (var i = 0; i < word.length; i++) {
-            answerArray[i] = "_";
-        }
         var remainingLetters = word.length;
+        var rightWord   
+        var wrongWord
+        // Counters
+        var winCount = 0;
+        var loseCount = 0;
+        var guessesLeft = 10; 
+        
 
-        while (remainingLetters > 0) {
+        console.log(word);
 
-            alert(answerArray.join(" "));
+        window.onload = function() {
+            makeBoard()
+        }
+       
+        
+   function makeBoard(){
+    for (var i = 0; i < word.length; i++) {
+        answerArray[i] = "_";
+    }
+    document.getElementById("boardgame").innerHTML = answerArray.join(" ");
+   }
+      
+
+  ;
+            // var guess = prompt("Guess a letter!");
+            // console.log(guess)
+
+
+            
+       // }
+
+        document.onkeyup = function(event) {
+                // while (remainingLetters > 0) {
+console.log( answerArray.join(" "))
 
 
 
 
-            var guess = prompt("Guess a letter!");
-            console.log(guess)
 
-
+            
+            var guess = event.key
             for (var j = 0; j < word.length; j++) {
                 if (word[j] === guess) {
                     answerArray[j] = guess;
-                    remainingLetters--;
+            
+                }
+                else {
+
                 }
             }
-        }
+            //refresh
+            document.getElementById("boardgame").innerHTML = answerArray.join(" ");
 
-    //     <!-- 1. Pick a random word.
-    // 2. Take the player’s guess.
+        }
+    
+
+        //Check the keypress against chosen word
+        //If it's match
+            //Go into answer array
+            //Show on page
+        //If it's not a match
+            //Go into wrong guess array
+            //show on page
+
+        //Win-getting all letter right before you run out of guesses
+        //Losses-if remaining guesses hits zero and still have underscore
+        
+
+
+
+
+
+           
+           
+           
+           
+           
+            //     <!-- 1. Pick a random word.
+            // 2. Take the player’s guess.
     // 3. Check that the player’s guess is a valid letter.
     // 4. Keep track of letters the player has guessed.
     // 5. Show the player their progress.
